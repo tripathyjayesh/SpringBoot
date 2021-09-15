b208dce31d1200601aeb7dc63ed44585bd51e3b5
Jayesh Tripathi <tripathyjayesh@gmail.com>
2021-09-16 02:26:17 +0530

    Add files via upload

---
+++ src/test/java/com/testcode/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
